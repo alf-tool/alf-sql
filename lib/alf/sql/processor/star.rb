4989f6c481c8faeab2656fa69dfcc3f9e67a500e
Bernard Lambeau <blambeau@gmail.com>
2013-09-27 12:04:21 +0200

    Avoid 'select *' without from_clause.

--- lib/alf/sql/processor/star.rb
+++ lib/alf/sql/processor/star.rb
@@ -4,7 +4,11 @@ module Alf
       class Star < Processor
 
         def on_select_exp(sexpr)
-          sexpr.with_update(:select_list, builder.select_star)
+          if sexpr.from_clause
+            sexpr.with_update(:select_list, builder.select_star)
+          else
+            sexpr
+          end
         end
 
       end # class Star
