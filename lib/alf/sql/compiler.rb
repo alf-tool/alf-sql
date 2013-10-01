0236ab428f2c433ed70a8c2ac0f9c63ead6c1c23
Bernard Lambeau <blambeau@gmail.com>
2013-10-01 09:53:37 +0200

    Follow alf-core about compilation plans.

--- lib/alf/sql/compiler.rb
+++ lib/alf/sql/compiler.rb
@@ -9,84 +9,88 @@ module Alf
 
       ### base
 
-      def on_leaf_operand(expr, &fallback)
+      def on_leaf_operand(plan, expr, &fallback)
         fresh_cog(expr, builder.select_all(expr.heading, expr.name))
       end
 
       ### non-relational
 
-      def on_clip(expr, compiled)
-        rewrite(compiled, expr, Processor::Clip, [expr.attributes, :is_table_dee])
+      def on_clip(plan, expr, compiled)
+        rewrite(plan, expr, compiled, Processor::Clip, [expr.attributes, :is_table_dee])
       end
 
-      def on_sort(expr, compiled)
+      def on_sort(plan, expr, compiled)
         if compiled.orderedby?(expr.ordering)
-          rebind(compiled, expr)
+          rebind(plan, expr, compiled)
         else
-          rewrite(compiled, expr, Processor::OrderBy, [expr.ordering])
+          rewrite(plan, expr, compiled, Processor::OrderBy, [expr.ordering])
         end
       end
 
       ### relational
 
-      def on_frame(expr, compiled)
-        compiled = _call(parser.sort(expr, expr.total_ordering), [ compiled ])
-        rewrite(compiled, expr, Processor::LimitOffset, [expr.limit, expr.offset])
+      def on_frame(plan, expr, compiled)
+        compiled = plan.compile{|p|
+          p.sort(expr.operand, expr.total_ordering)
+        }
+        rewrite(plan, expr, compiled, Processor::LimitOffset, [expr.limit, expr.offset])
       end
 
-      def on_intersect(expr, left, right)
-        rewrite(left, expr, Processor::Merge, [:intersect, right.sexpr])
+      def on_intersect(plan, expr, left, right)
+        rewrite(plan, expr, left, Processor::Merge, [:intersect, right.sexpr])
       end
 
-      def on_join(expr, left, right)
-        rewrite(left, expr, Processor::Join, [right.sexpr])
+      def on_join(plan, expr, left, right)
+        rewrite(plan, expr, left, Processor::Join, [right.sexpr])
       end
 
-      def on_matching(expr, left, right)
-        rewrite(left, expr, Processor::SemiJoin, [right.sexpr, false])
+      def on_matching(plan, expr, left, right)
+        rewrite(plan, expr, left, Processor::SemiJoin, [right.sexpr, false])
       end
 
-      def on_minus(expr, left, right)
-        rewrite(left, expr, Processor::Merge, [:except, right.sexpr])
+      def on_minus(plan, expr, left, right)
+        rewrite(plan, expr, left, Processor::Merge, [:except, right.sexpr])
       end
 
-      def on_not_matching(expr, left, right)
-        rewrite(left, expr, Processor::SemiJoin, [right.sexpr, true])
+      def on_not_matching(plan, expr, left, right)
+        rewrite(plan, expr, left, Processor::SemiJoin, [right.sexpr, true])
       end
 
-      def on_page(expr, compiled)
+      def on_page(plan, expr, compiled)
         index, size = expr.page_index, expr.page_size
-        #
-        ordering = expr.total_ordering
-        ordering = ordering.reverse if index < 0
-        #
-        compiled = _call(parser.sort(expr, ordering), [ compiled ])
-        rewrite(compiled, expr, Processor::LimitOffset, [size, (index.abs - 1) * size])
+        compiled = plan.compile{|p|
+          ordering = expr.total_ordering
+          ordering = ordering.reverse if index < 0
+          p.sort(expr.operand, ordering)
+        }
+        rewrite(plan, expr, compiled, Processor::LimitOffset, [size, (index.abs - 1) * size])
       end
 
-      def on_project(expr, compiled)
+      def on_project(plan, expr, compiled)
+        compiled = plan.compile{|p|
+          p.clip(expr.operand, expr.stay_attributes)
+        }
         preserving = expr.key_preserving? rescue false
-        #
-        compiled = rewrite(compiled, expr, Processor::Distinct) unless preserving
-        compiled = self._call(parser.clip(expr, expr.stay_attributes), [ compiled ])
-        #
-        rebind(compiled, expr)
+        if not(preserving) and not(expr.stay_attributes.empty?)
+          compiled = rewrite(plan, expr, compiled, Processor::Distinct)
+        end
+        rebind(plan, expr, compiled)
       end
 
-      def on_rename(expr, compiled)
-        rewrite(compiled, expr, Processor::Rename, [expr.renaming])
+      def on_rename(plan, expr, compiled)
+        rewrite(plan, expr, compiled, Processor::Rename, [expr.renaming])
       end
 
-      def on_restrict(expr, compiled)
-        rewrite(compiled, expr, Processor::Where, [expr.predicate])
+      def on_restrict(plan, expr, compiled)
+        rewrite(plan, expr, compiled, Processor::Where, [expr.predicate])
       end
 
-      # def on_summarize(expr, compiled)
+      # def on_summarize(plan, expr, compiled)
       #   # -> SQL's GROUP-BY
       # end
 
-      def on_union(expr, left, right)
-        rewrite(left, expr, Processor::Merge, [:union, right.sexpr])
+      def on_union(plan, expr, left, right)
+        rewrite(plan, expr, left, Processor::Merge, [:union, right.sexpr])
       end
 
     protected
@@ -95,12 +99,12 @@ module Alf
         Cog.new(expr, self, sexpr)
       end
 
-      def rewrite(compiled, expr, processor, args = [])
+      def rewrite(plan, expr, compiled, processor, args = [])
         rewrited = processor.new(*args.push(builder)).call(compiled.sexpr)
         Cog.new(expr, self, rewrited)
       end
 
-      def rebind(compiled, expr)
+      def rebind(plan, expr, compiled)
         Cog.new(expr, self, compiled.sexpr)
       end
 
